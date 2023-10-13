dcfdcb7dda6ac5641a46d9c8884f7437e881c8e2
RogeMattos <rogemattos.rm@gmail.com>
2023-10-13 20:59:59 -0300

    feat(navbar):criacao-navbar

--- LABSchool-Manager/src/app/services/auth.service.ts
+++ LABSchool-Manager/src/app/services/auth.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private currentUserSubject: BehaviorSubject<string | null>;
+  public currentUser: Observable<string | null>;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<string | null>(this.getLocalUser());
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  private getLocalUser(): string | null {
+    const user = localStorage.getItem('currentUser');
+    return user ? JSON.parse(user).username : null; // Usando username para o currentUser
+  }
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('currentUser');
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
+
 }
