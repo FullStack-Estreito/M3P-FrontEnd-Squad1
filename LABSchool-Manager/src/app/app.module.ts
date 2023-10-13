c67eaa917858e26241808bb7c0aaafbf0b9cdf59
“FeReDragon” <f3030ma@gmail.com>
2023-10-13 11:22:09 -0300

    feature/modulos-components-inicial

--- LABSchool-Manager/src/app/app.module.ts
+++ LABSchool-Manager/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateEditComponent } from './Atendimentos/components/create-edit/create-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ LABSchool-Manager/src/app/exercicios/exercicios.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CreateEditComponent } from './components/create-edit/create-edit.component';
+
+
+
+@NgModule({
+  declarations: [
+    CreateEditComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ExerciciosModule { }
