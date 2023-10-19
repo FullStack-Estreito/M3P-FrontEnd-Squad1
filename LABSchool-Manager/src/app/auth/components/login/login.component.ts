a1cf6797aca87edb1a428c10c61e0b4e957ece78
“FeReDragon” <f3030ma@gmail.com>
2023-10-19 18:32:12 -0300

    feat(auth): login-autenticacao

--- LABSchool-Manager/src/app/auth/components/login/login.component.ts
+++ LABSchool-Manager/src/app/auth/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+ 
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
+  login(): void {
+    console.log('Tentativa de login iniciada com', this.email); // Log 1
+
+   
+    this.authService.login(this.email, this.password).subscribe(
+      (authenticated: boolean) => {
+        console.log('Autenticado:', authenticated); // Log 2
+
+       
+        if (authenticated) {
+          this.router.navigate(['/dashbord']);
+        } else {
+          this.errorMessage = 'Credenciais inválidas. Por favor, tente novamente.';
+        }
+      },
+      (error: any) => {
+        console.error('Erro ao tentar autenticar:', error); // Log 3
+      
+        this.errorMessage = error;
+      }
+    );
+  }
 }
+
