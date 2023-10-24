e7e6b17ba7b713e1f793f4a55a33165a450597dc
“FeReDragon” <f3030ma@gmail.com>
2023-10-23 22:14:38 -0300

    feat(exercicio): logica-component-model-service

--- LABSchool-Manager/src/app/services/exercicios.service.ts
+++ LABSchool-Manager/src/app/services/exercicios.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Exercicio, TipoUsuario } from '../exercicios/model/exercicios.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExerciciosService {
 
-  constructor() { }
+  private apiUrl: string = "http://localhost:5203/api";
+
+  constructor(private http: HttpClient) { }
+
+   // Método para obter alunos
+   getAlunos(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/usuario`).pipe(
+      map(users => {
+        const alunos = users.filter(user => user.tipoUsuario === TipoUsuario.Aluno);
+        console.log("Alunos filtrados: ", alunos);
+        return alunos;
+      })
+    );
+  }
+
+  createExercicio(exercicio: Exercicio): Observable<Exercicio> {
+    return this.http.post<Exercicio>(`${this.apiUrl}/exercicio`, exercicio);
+  }
+
+  getExercicios(): Observable<Exercicio[]> {
+    return this.http.get<Exercicio[]>(`${this.apiUrl}/exercicio`);
+  }
+
+  updateExercicio(id: number, exercicio: Exercicio): Observable<Exercicio> {
+    return this.http.put<Exercicio>(`${this.apiUrl}/exercicio/${id}`, exercicio);
+  }
+
+  deleteExercicio(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/exercicio/${id}`);
+  }
 }
+
