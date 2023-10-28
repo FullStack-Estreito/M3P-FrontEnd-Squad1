59ae34fd75c692993a272bcebacfb83b85015313
“FeReDragon” <f3030ma@gmail.com>
2023-10-28 18:11:52 -0300

    feat(core/service): currentUser-logout

--- LABSchool-Manager/src/app/core/components/sidebar/sidebar.component.ts
+++ LABSchool-Manager/src/app/core/components/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';  // Importe o Router
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';  // Importe o AuthService
 
 @Component({
   selector: 'app-sidebar',
@@ -8,12 +9,17 @@ import { Router } from '@angular/router';  // Importe o Router
 })
 export class SidebarComponent {
 
-  // Injete o Router no construtor
-  constructor(private router: Router) { }
+  // Injete tanto o Router quanto o AuthService no construtor
+  constructor(private router: Router, private authService: AuthService) { }
 
   // Método para navegar programaticamente
   navigateTo(route: string) {
     this.router.navigate([route]);
   }
-}
 
+  // Método de Logout
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
