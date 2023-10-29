47a7bb223508d2d46e7cb5feb1d0146d64650c96
“FeReDragon” <f3030ma@gmail.com>
2023-10-29 14:48:06 -0300

    feat(dashbord): chart-bar

--- LABSchool-Manager/src/app/core/components/dashboard/dashboard.component.ts
+++ LABSchool-Manager/src/app/core/components/dashboard/dashboard.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit, Pipe, PipeTransform } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, Pipe, PipeTransform } from '@angular/core';
 import { DashboardService } from '../../../services/dashboard.service'; 
 import { User } from '../../../usuarios/Model/user.model';
 import { Router } from '@angular/router';
+import { Atendimento } from 'src/app/Atendimentos/model/atendimento.moel';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-
 export class DashboardComponent implements OnInit {
-
+  
   usuarios: any[] = [];
   totalAlunos: number = 0; 
   totalExercicios: number = 0;
@@ -19,6 +20,10 @@ export class DashboardComponent implements OnInit {
   alunos: User[] = [];
   buscaUsuarios: any[] = [];
   buscaAlunos: User[] = [];
+  atendimentosPorDia: any[] = [];
+  atendimentosChart: any;
+
+  @ViewChild('chartRef') chartRef: ElementRef | undefined;
 
   constructor(private dashboardService: DashboardService, private router: Router) { }
 
@@ -34,7 +39,60 @@ export class DashboardComponent implements OnInit {
     this.dashboardService.getTotalAtendimentos().subscribe(total => this.totalAtendimentos = total);
     this.dashboardService.getAlunos().subscribe(alunos => this.alunos = alunos);
     this.dashboardService.getUsuarios().subscribe(usuarios => this.usuarios = usuarios);
-}
+    this.dashboardService.getAtendimentos().subscribe(atendimentos => {
+      this.processAtendimentosData(atendimentos);
+      this.initializeChart(); 
+    });
+    
+
+    this.initializeChart();
+  }
+
+  private processAtendimentosData(atendimentos: Atendimento[]): void {
+    const counts: { [key: string]: number } = {};
+  
+    atendimentos.forEach(atendimento => {
+      const date = new Date(atendimento.dataHora).toLocaleDateString();
+      counts[date] = (counts[date] || 0) + 1;
+    });
+  
+    this.atendimentosPorDia = Object.keys(counts).map(date => ({
+      date: date,
+      count: counts[date]
+    }));
+  }
+
+  private initializeChart(): void {
+    if (this.atendimentosPorDia && this.chartRef) {
+      const ctx = this.chartRef.nativeElement.getContext('2d');
+      
+      if (this.atendimentosChart) {
+        this.atendimentosChart.destroy();  
+      }
+  
+      this.atendimentosChart = new Chart(ctx, {
+        type: 'bar',  
+        data: {
+          labels: this.atendimentosPorDia.map(d => d.date),
+          datasets: [{
+            label: 'Atendimentos por Dia',
+            data: this.atendimentosPorDia.map(d => d.count),
+            backgroundColor: '#00BFFF',  
+            borderColor: '#3cba9f',
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      });
+    }
+  }
+
 
 
   onSearch(query: string): void {
