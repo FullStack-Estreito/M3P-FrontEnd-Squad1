5487d61e4df7638bf8cc39a6c1c16280684ff2f8
“FeReDragon” <f3030ma@gmail.com>
2023-10-29 22:02:18 -0300

    feat(service): update current - user-isAdmin

--- LABSchool-Manager/src/app/services/auth.service.ts
+++ LABSchool-Manager/src/app/services/auth.service.ts
@@ -2,42 +2,70 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
+import { TipoUsuario } from '../usuarios/Model/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private readonly API_URL = 'http://localhost:5203/api/usuario';
-  private currentUserSubject: BehaviorSubject<any | null>;
+  public currentUserSubject: BehaviorSubject<any | null>;
   public currentUser: Observable<any | null>;
 
   constructor(private http: HttpClient) {
     const currentUserString = localStorage.getItem('currentUser');
-    this.currentUserSubject = new BehaviorSubject<any | null>(currentUserString ? JSON.parse(currentUserString) : null);
+    const parsedUser = currentUserString ? JSON.parse(currentUserString) : null;
+    
+    // Log para verificar o tipoUsuario depois que o usuário estiver logado
+    if (parsedUser) {
+        console.log('Usuário logado com tipoUsuario:', parsedUser.tipoUsuario);
+    }
+
+    this.currentUserSubject = new BehaviorSubject<any | null>(parsedUser);
     this.currentUser = this.currentUserSubject.asObservable();
-  }
+}
+
 
   login(username: string, password: string): Observable<boolean> {
     return this.http.post<any>(`${this.API_URL}/login`, { Email: username, password })
       .pipe(
         map(response => {
+          console.log('Resposta completa do servidor:', response); // Log para a resposta completa
+
           if (response && response.token) {
+            console.log('Token recebido:', response.token); // Log para o token
+        
             const user = {
-              nome: response.nome, // Alterado para 'nome'
-              token: response.token
+              nome: response.nome, 
+              token: response.token,
+              tipoUsuario: response.tipoUsuario, // Ajuste aqui
             };
+        
             localStorage.setItem('currentUser', JSON.stringify(user));
             this.currentUserSubject.next(user);
             return true;
-          }
+        }
+        
           return false;
         }),
         catchError((error) => {
           console.error('Erro ao tentar fazer login:', error);
-          return throwError('Credenciais inválidas. Por favor, tente novamente.');
+          return throwError(error.message || 'Credenciais inválidas. Por favor, tente novamente.');
         })
       );
+}
+
+
+  isAdmin(): boolean {
+    const user = localStorage.getItem('currentUser') ? JSON.parse(localStorage.getItem('currentUser')!) : null;
+    console.log('Valor de user.tipoUsuario:', user?.tipoUsuario);
+    return user && user.tipoUsuario === TipoUsuario.Administrador;
   }
+
+  getCurrentUserValue(): any {
+    return this.currentUserSubject.value;
+}
+
 
   logout() {
     localStorage.removeItem('currentUser');
